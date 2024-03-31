12cf764f0978be744b2d2e5580fb59643cdf03de
李昀宸 <b09705015@ntu.edu.tw>
2024-03-31 16:39:33 +0800

    fix(app): revise workflow

--- src/modules/entry/services/index.ts
+++ src/modules/entry/services/index.ts
@@ -2,7 +2,7 @@ import { signIn } from "next-auth/react";
 
 export const signInGithub = async () => {
   try {
-    await signIn("github", { callbackUrl: "http://localhost:3000/post" });
+    await signIn("github", { callbackUrl: "/post" });
   } catch (error) {
     console.log(error);
   }
